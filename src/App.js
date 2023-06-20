d5fba1a6c72da931945d90d7c4d95e056ac1fb2a
Taisei <taisei96.web@gmail.com>
2023-06-20 09:10:59 +0900

    新しいノートを追加する関数

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import "./index.css";
 import Sidebar from "./components/Sidebar";
 
 function App() {
+
+  const [notes, setNotes] = useState([]);
+
+  const onAddNote = () => {
+    console.log("新しく追加されました")
+
+    const newNote = {
+      id: 1,
+      title: "新しいノート",
+      content: "新しいノートの内容",
+      modDate: Date.now(),
+    }
+
+    setNotes([...notes, newNote]);
+    console.log(notes);
+  } 
+
   return (
     <div className="App">
-      <Sidebar />
+      <Sidebar onAddNote={onAddNote} />
     </div>
-  )
+  );
 }
 
 export default App;
